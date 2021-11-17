00084e847f29a60c4dded99e4bd526b3d502a084
Michael Wong <michaelwong1391@gmail.com>
2021-11-17 14:49:19 -0500

    adding credits works, account balances are displaying correctly but js may have issues with its number system

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -7,10 +7,8 @@ import CreditsPage from './components/CreditsPage';
 import DebitsPage from './components/DebitsPage';
 
 class App extends Component {
-
   constructor() {
     super();
-
     this.state = {
       accountBalance: 0,
       currentUser: {
@@ -21,27 +19,26 @@ class App extends Component {
       credits: []
     }
   }
-
   addDebit = (newDebit) => {
-    this.setState({debits: [...this.state.debits, newDebit]})
-    this.setState({accountBalance: this.state.accountBalance - parseFloat(newDebit.amount)})
+    this.setState({debits: [...this.state.debits, newDebit]});
+    console.log(newDebit);
+    this.setState({accountBalance: this.state.accountBalance - parseFloat(newDebit.amount)});
   }
-
-  addCredit() {
-
+  addCredit = (newCredit) => {
+    this.setState({credits: [...this.state.credits, newCredit]});
+    this.setState({accountBalance: this.state.accountBalance + parseFloat(newCredit.amount)});
   }
-
-
   componentDidMount() {
     fetch("https://moj-api.herokuapp.com/debits")
       .then(res => res.json())
       .then(
         (result) => {
-          this.setState({debits: result})
+          this.setState({debits: result});
           for (let i = 0; i < result.length; i++)
           {
             let amount = result[i].amount;
-            this.setState({accountBalance: this.state.accountBalance - amount})
+            this.setState({accountBalance: this.state.accountBalance - amount});
+            // console.log(this.state.accountBalance)
           }
         }
       );
@@ -49,27 +46,28 @@ class App extends Component {
       .then(res => res.json())
       .then(
         (result) => {
-          this.setState({credits: result})
+          this.setState({credits: result});
+          for (let i = 0; i < result.length; i++)
+          {
+            let amount = result[i].amount;
+            this.setState({accountBalance: this.state.accountBalance + amount});
+          }
         }
       );
   }
-
   render() {
-
     let mockLogIn = (logInInfo) => {
       const newUser = {...this.state.currentUser}
       newUser.userName = logInInfo.userName
       this.setState({currentUser: newUser})
     }
-
-    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance.toFixed(2)} />);
     const UserProfileComponent = () => (
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={mockLogIn} />)
-    const CreditsComponent = () => (<CreditsPage credits={this.state.credits} />)
+    const CreditsComponent = () => (<CreditsPage credits={this.state.credits} handleNewCredit={this.addCredit} balance={this.state.accountBalance} />)
     const DebitsComponent = () => (<DebitsPage debits={this.state.debits} handleNewDebit={this.addDebit} balance={this.state.accountBalance} />)
-
     return (
       <Router>
         <Switch>
@@ -84,7 +82,6 @@ class App extends Component {
       </Router>
     );
   }
-
 }
 
 export default App;
--- bank-of-react/src/components/CreditsPage.js
+++ bank-of-react/src/components/CreditsPage.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 class CreditsPage extends Component {
   componentDidMount() {
     let credits_div = document.getElementById("credits");
@@ -17,11 +15,46 @@ class CreditsPage extends Component {
       credits_div.appendChild(credit_amount);
     }
   }
+  addCredit = () => {
+    let amount = document.getElementById("amount");
+      let dollar = parseFloat(amount.value).toFixed(2);
+      if (dollar === "NaN")
+      {
+         alert("Please enter correct amount")
+         return;
+      }
+      let credits_div = document.getElementById("credits");
+      let credit = document.createElement("p");
+      credit.style.fontWeight = "bold";
+      let description = document.getElementById("credit");
+      credit.innerHTML = `${description.value}`
+      let credit_amount = document.createElement("p");
+      let date = `${new Date()}`;
+      credit_amount.innerHTML = `Amount: ${dollar} Date: ${date}`;
+      credits_div.appendChild(credit);
+      credits_div.appendChild(credit_amount);
+      let newcredit = {
+         description: description.value,
+         amount: dollar,
+         date: date
+      }
+      this.props.handleNewCredit(newcredit);
+  }
 
   render() {
     return (
       <div style={{ textAlign: "center" }}>
         <h1 style={{ fontSize: "50px" }}>Credits</h1>
+        <p>Balance: {this.props.balance.toFixed(2)}</p>
+        <div>
+          <p>Credit Description</p>
+          <input id="credit"></input>
+          <p>Amount</p>
+          <input id="amount"></input>
+          <br></br>
+          <br></br>
+          <button onClick={this.addCredit}>Submit</button>
+        </div>
         <br></br>
         <Link to="/">Return to Home</Link>
         <br></br>
